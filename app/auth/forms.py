0d1ca6f3b73b1d3bc37a3e4d7a2b6edbe3c93cac
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-15 00:45:33 +0300

    creatin a form instance and passing it to the template

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,3 +9,12 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
     submit = SubmitField('Sign Up')
+
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
