6d83373cd23a75945d1062e924d60d7627860dcd
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-14 13:08:54 +0300

    adding properties to the table blogs

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from . import db
 
 class Blog(db.Model):
+    __tablename__="blogs"
+    
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Blog {self.post}'
