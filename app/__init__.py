b62b286d8dbcfd10c3178b3a80f059d2d493c3d8
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-14 18:57:29 +0300

    creating a user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -13,8 +17,12 @@ def create_app(config_name):
 
     app.config.from_object(config_options[config_name])
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
