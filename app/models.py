dccd30f48ffd5b13f41ac119237ccc70c7b90b5f
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-14 18:46:54 +0300

    adding password secure and doing second migration

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 
@@ -12,7 +13,20 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    password_secure = db.Column(db.String(255))
     blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
 
     
     def __repr__(self):
