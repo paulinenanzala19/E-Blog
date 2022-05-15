866a0cc9bdba5d0a62dd0bb6cb2c498e8e4a86c2
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-15 16:48:46 +0300

    adding functionality to the upvote and downvote

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..models import Blog,User
+from ..models import Blog,User,Upvote,Downvote
 from ..request import get_quotes
 from flask_login import login_required,current_user
 from .forms import BlogForm,UpdateProfile
@@ -79,3 +79,36 @@ def upload_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/like/<int:id>',methods = ['POST','GET'])
+@login_required
+def likes(id):
+    get_blogs = Upvote.get_upvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for blog in get_blogs:
+        to_str = f'{blog}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_vote = Upvote(user = current_user, blog_id=id)
+    new_vote.save_upvote()
+    return redirect(url_for('main.index',id=id))
+
+@main.route('/dislike/<int:id>',methods = ['POST','GET'])
+@login_required
+def dislikes(id):
+    blog = Downvote.get_downvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for b in blog:
+        to_str = f'{p}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_downvote = Downvote(user = current_user, blog_id=id)
+    new_downvote.save_downvote()
+    return redirect(url_for('main.index',id = id))
+
