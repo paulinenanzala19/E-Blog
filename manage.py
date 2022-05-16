7c9e409845011e11e4e91874e26543aa709c924a
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-16 19:11:11 +0300

    adding test file

--- manage.py
+++ manage.py
@@ -4,12 +4,21 @@ from app.models import Blog,User
 from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('production')
+app=create_app('test')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 
 @manager.shell
---
+++ tests/test_blog.py
@@ -0,0 +1,28 @@
+import unittest
+from app.models import Blog, User,
+from app import db
+
+
+class BlogModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_Jessy = User(username='jessy', password='pipis', email='test@test.com')
+        self.new_blog = Blog(id=1, title='Test', post='test blog', user_id=self.user_jessy.id)
+
+    def tearDown(self):
+        Blog.query.delete()
+        User.query.delete()
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_blog.title, 'Test')
+        self.assertEquals(self.new_blog.post, ' A test blog')
+        self.assertEquals(self.new_blog.user_id, self.user_jessy.id)
+
+    def test_save_blog(self):
+        self.new_blog.save()
+        self.assertTrue(len(Blog.query.all()) > 0)
+
+    def test_get_blog(self):
+        self.new_blog.save()
+        got_blog = Blog.get_blog(1)
+        self.assertTrue(get_blog is not None)
+
